d048c6c385914ca10284f3cf0f0d9566e7bf149e
Chris Conlon <christopherconlon@gmail.com>
2021-11-05 15:22:27 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kiltsreader-pkg-chrisconlon",
+    version="0.0.1",
+    author="Christopher Conlon and Chitra Marti",
+    author_email="cconlon@stern.nyu.edu",
+    description="A package for reading Kilts NielsenIQ data files and directories",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chrisconlon/kiltsnielsen",
+    project_urls={
+        "Bug Tracker": "https://github.com/chrisconlon/kiltsnielsen/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
