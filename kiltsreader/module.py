2eb24e675b9076ca7216fd248180d84a3a769f65
Chris Conlon <christopherconlon@gmail.com>
2022-10-16 22:22:04 -0400

    fix panelist types

--- kiltsreader/module.py
+++ kiltsreader/module.py
@@ -124,6 +124,10 @@ dict_types = {'upc': pa.uint64(),
               'quantity':pa.uint16(),
               'household_code':pa.uint32(),
               'Household_Cd':pa.uint32(),
+              'Fips_County_Desc':pa:string(),
+              'Fips_State_Desc':pa:string(),
+              'Scantrack_Market_Identifier_Desc':pa:string(),
+              'DMA_Name':pa:string(),
               }
 
 
@@ -1038,10 +1042,6 @@ class PanelReader(object):
         self.df_panelists = []
         self.df_trips = []
         self.df_purchases = []
-
-        #self.df_panelists = pd.DataFrame()
-        #self.df_trips = pd.DataFrame()
-        #self.df_purchases = pd.DataFrame()
 
         self.df_extra = pd.DataFrame()
 
